1a612c262f42c89f3548b71f42a24b85a5f4d8c2
Bartłomiej Kawa <kawaxbart@gmail.com>
2020-10-03 17:01:07 +0200

    Add spinning circle

---
+++ Assets/Scripts/CircleSpinner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleSpinner : MonoBehaviour
+{
+    float velocity;
+    float acceleration;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        velocity = Time.fixedDeltaTime;
+        acceleration = Time.fixedDeltaTime;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Rotate(Vector3.forward, velocity);
+        velocity += acceleration;
+    }
+}
