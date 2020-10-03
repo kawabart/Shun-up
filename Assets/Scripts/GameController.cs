6c801683ad594e5fb755cdaf96d298bcba9c49cc
Bartłomiej Kawa <kawaxbart@gmail.com>
2020-10-03 17:26:50 +0200

    Add game controller

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField]
+    private CircleSpinner circleSpinner;
+    private bool isPlaying = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isPlaying && Input.anyKeyDown)
+        {
+            isPlaying = true;
+            circleSpinner.enabled = true;
+        }
+    }
+}
